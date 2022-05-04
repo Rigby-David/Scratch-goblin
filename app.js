8b346c03244d4af4be21771f0f9e791b2402b769
David <Rigby335@hotmail.com>
2022-05-04 16:40:23 -0700

    grabbed el by id and let state

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 // import functions and grab DOM elements
+const defeatedNumberEl = document.querySelector('#defeated-number');
+const adventurerHPEl = document.querySelector('#adventurer-HP');
+const adventurerImgEl = document.querySelector('#adventurer-img');
+const form = document.querySelector('form');
+const goblinListEl = document.querySelector('.goblins');
 
 // let state
-
+let defeatedGoblinsCount = 0;
+let playerHP = 10;
+let goblins = [
+    { id: 1, name: McStabby, hp: 2},
+    { id: 2, name: LordStabbo, hp: 4},
+];
 // set event listeners 
   // get user input
   // use user input to update state 
